auyer/public-inbox

commit ddf589a5c12daa561568c73f902cd7ecd402b643

ipc: recv_cmd returns IO handles instead of FDs
Returning FDs is error prone and can lead to FD leaks if the
caller fails to map them to Perl IO handles.  So do the FD to
PerlIO conversion at a lower level to simplify callers.  It
seems safe enough to always treat returned FDs as O_RDWR for the
`open' perlop, as we've been doing that in lei for years.

lib/PublicInbox/Spawn.pm
@@ -254,7 +254,7 @@ SV *send_cmd4_(PerlIO *s, SV *sio, SV *data, int flags, long tries)
 	return sent >= 0 ? newSViv(sent) : &PL_sv_undef;
 }
 
-void recv_cmd4(PerlIO *s, SV *buf, STRLEN n)
+void recv_cmd4_(PerlIO *s, SV *buf, STRLEN n)
 {
 	union my_cmsg cmsg = { 0 };
 	struct msghdr msg = { 0 };
@@ -348,6 +348,11 @@ EOM
 		%RLIMITS = rlimit_map();
 		*send_cmd4 = sub ($$$$;$) {
 			send_cmd4_($_[0], $_[1], $_[2], $_[3], $_[4] // 50);
+		};
+		require PublicInbox::CmdIPC4;
+		*recv_cmd4 = sub ($$$) {
+			my @r = recv_cmd4_($_[0], $_[1], $_[2]);
+			defined($r[0]) ? PublicInbox::CmdIPC4::fd2io(@r) : @r;
 		}
 	} else {
 		require PublicInbox::SpawnPP;

lib/PublicInbox/CmdIPC4.pm
@@ -18,6 +18,8 @@ sub sendmsg_retry ($) {
 	1;
 }
 
+sub fd2io (@) { map { open my $fh, '+<&=', $_; $fh } @_ }
+
 BEGIN { eval {
 require Socket::MsgHdr; # XS
 no warnings 'once';
@@ -50,7 +52,7 @@ no warnings 'once';
 	$_[1] = $mh->buf;
 	return () if $r == 0;
 	my (undef, undef, $data) = $mh->cmsghdr;
-	defined($data) ? unpack('i' x (length($data) / 4), $data) : ();
+	defined($data) ? fd2io(unpack('i' x (length($data) / 4), $data)) : ();
 };
 
 } } # /eval /BEGIN

lib/PublicInbox/IPC.pm
@@ -327,15 +327,11 @@ sub ipc_sibling_atfork_child {
 
 sub recv_and_run {
 	my ($self, $s2, $len, $full_stream) = @_;
-	my @fds = $recv_cmd->($s2, my $buf, $len // $MY_MAX_ARG_LEN);
-	return if scalar(@fds) && !defined($fds[0]);
+	my @io = $recv_cmd->($s2, my $buf, $len // $MY_MAX_ARG_LEN);
+	return if scalar(@io) && !defined($io[0]);
 	my $n = length($buf) or return 0;
-	my $nfd = 0;
-	for my $fd (@fds) {
-		open(my $cmdfh, '+<&=', $fd);
-		$self->{$nfd++} = $cmdfh;
-		$cmdfh->autoflush(1);
-	}
+	@$self{0..$#io} = @io;
+	$_->autoflush(1) for @io;
 	while ($full_stream && $n < $len) {
 		my $r = sysread($s2, $buf, $len - $n, $n);
 		if ($r) {
@@ -356,7 +352,7 @@ sub recv_and_run {
 	my $sub = shift @$args;
 	eval { $self->$sub(@$args) };
 	warn "$$ $0 wq_worker: $sub: $@" if $@;
-	delete @$self{0..($nfd-1)};
+	delete @$self{0..$#io};
 	$n;
 }
 

lib/PublicInbox/LEI.pm
@@ -1156,21 +1156,21 @@ sub accept_dispatch { # Listener {post_accept} callback
 	my ($sock) = @_; # ignore other
 	$sock->autoflush(1);
 	my $self = bless { sock => $sock }, __PACKAGE__;
-	my (@fds, $buf);
+	my (@io, $buf);
 	do {
 		poll_in $sock, 60_000 or return send_gently $sock,
 						'timed out waiting to recv FDs';
 		# (4096 * 33) >(old) MY_MAX_ARG_LEN, TODO: 64K
-		@fds = $PublicInbox::IPC::recv_cmd->($sock, $buf, 4096 * 33)
+		@io = $PublicInbox::IPC::recv_cmd->($sock, $buf, 4096 * 33)
 			or return; # EOF
-	} while (!defined($fds[0]) && $! == EAGAIN);
-	if (!defined($fds[0])) {
+	} while (!defined($io[0]) && $! == EAGAIN);
+	if (!defined($io[0])) {
 		warn(my $msg = "recv_cmd failed: $!");
 		return send_gently $sock, $msg;
 	} else {
-		my $i = 0;
-		open($self->{$i++}, '+<&=', $_) for @fds;
-		$i == 4 or return send_gently $sock, 'not enough FDs='.($i-1);
+		@io == 4 or return
+			send_gently $sock, 'not enough FDs='.scalar(@io);
+		@$self{0..$#io} = @io;
 	}
 	# $ENV_STR = join('', map { "\0$_=$ENV{$_}" } keys %ENV);
 	# $buf = "$argc\0".join("\0", @ARGV).$ENV_STR."\0\0";
@@ -1198,14 +1198,13 @@ sub event_step {
 	local %ENV = %{$self->{env}};
 	local $current_lei = $self;
 	eval {
-		my @fds = $PublicInbox::IPC::recv_cmd->(
+		my @io = $PublicInbox::IPC::recv_cmd->(
 			$self->{sock} // return, my $buf, 4096);
-		if (scalar(@fds) == 1 && !defined($fds[0])) {
+		if (scalar(@io) == 1 && !defined($io[0])) {
 			return if $! == EAGAIN;
 			die "recvmsg: $!" if $! != ECONNRESET;
-			@fds = (); # for open loop below:
+			@io = (); # for open loop below:
 		}
-		for (@fds) { open my $rfh, '+<&=', $_ }
 		if ($buf eq '') {
 			_drop_wq($self); # EOF, client disconnected
 			dclose($self);

lib/PublicInbox/LeiSelfSocket.pm
@@ -20,16 +20,15 @@ sub new {
 
 sub event_step {
 	my ($self) = @_;
-	my ($buf, @fds);
-	@fds = $PublicInbox::IPC::recv_cmd->($self->{sock}, $buf, 4096 * 33);
-	if (scalar(@fds) == 1 && !defined($fds[0])) {
+	my ($buf, @io);
+	@io = $PublicInbox::IPC::recv_cmd->($self->{sock}, $buf, 4096 * 33);
+	if (scalar(@io) == 1 && !defined($io[0])) {
 		return if $!{EAGAIN};
 		die "recvmsg: $!" unless $!{ECONNRESET};
-	} else { # just in case open so perl can auto-close them:
-		for (@fds) { open my $fh, '+<&=', $_ };
 	}
 	return $self->close if $buf eq '';
-	warn 'W: unexpected self msg: ', git_quote($buf), " fds=(@fds)\n";
+	warn 'W: unexpected self msg: ', git_quote($buf),
+		' nfds=', scalar(@io), "\n";
 	# TODO: figure out what to do with these messages...
 }
 

lib/PublicInbox/Syscall.pm
@@ -580,7 +580,8 @@ require PublicInbox::CmdIPC4;
 					$cmsghdr);
 		if ($lvl == SOL_SOCKET && $type == SCM_RIGHTS) {
 			$len -= CMSG_ALIGN_SIZEOF_cmsghdr;
-			@ret = @fds[0..(($len / SIZEOF_int) - 1)];
+			@ret = PublicInbox::CmdIPC4::fd2io(
+					@fds[0..(($len / SIZEOF_int) - 1)]);
 		}
 	}
 	@ret;

lib/PublicInbox/XapHelper.pm
@@ -271,19 +271,18 @@ sub recv_loop {
 	local $SIG{USR1} = \&reopen_logs;
 	while (defined($in)) {
 		PublicInbox::DS::sig_setmask($workerset);
-		my @fds = eval { # we undef $in in SIG{TERM}
+		my @io = eval { # we undef $in in SIG{TERM}
 			$PublicInbox::IPC::recv_cmd->($in, $rbuf, 4096*33)
 		};
 		if ($@) {
 			exit if !$in; # hit by SIGTERM
 			die;
 		}
-		scalar(@fds) or exit(66); # EX_NOINPUT
-		die "recvmsg: $!" if !defined($fds[0]);
+		scalar(@io) or exit(66); # EX_NOINPUT
+		die "recvmsg: $!" if !defined($io[0]);
 		PublicInbox::DS::block_signals(POSIX::SIGALRM);
 		my $req = bless {}, __PACKAGE__;
-		my $i = 0;
-		open($req->{$i++}, '+<&=', $_) for @fds;
+		@$req{0..$#io} = @io;
 		$req->{1}->autoflush(1) if $req->{1};
 		local $stderr = $req->{1} // \*STDERR;
 		die "not NUL-terminated" if chop($rbuf) ne "\0";

script/lei
@@ -34,15 +34,11 @@ my $sigchld = sub {
 };
 my @parent;
 my $exec_cmd = sub {
-	my ($fds, $argc, @argv) = @_;
+	my ($io, $argc, @argv) = @_;
 	my $parent = $$;
 	require POSIX;
 	my @old = (\*STDIN, \*STDOUT, \*STDERR);
-	my @rdr;
-	for my $fd (@$fds) {
-		open(my $newfh, '+<&=', $fd) or die "open +<&=$fd: $!";
-		push @rdr, shift(@old), $newfh;
-	}
+	my @rdr = map { (shift(@old), $_) } @$io;
 	my $do_exec = sub {
 		my @non_std; # ex. $op_p from lei_edit_search
 		while (my ($io, $newfh) = splice(@rdr, 0, 2)) {
@@ -64,14 +60,14 @@ my $exec_cmd = sub {
 	$SIG{CHLD} = $sigchld;
 	my $pid = fork // die "fork: $!";
 	if ($pid == 0) {
-		$do_exec->() if $fds->[1]; # git-credential, pager
+		$do_exec->() if $io->[1]; # git-credential, pager
 
 		# parent backgrounds on MUA
 		POSIX::setsid() > 0 or die "setsid: $!";
 		@parent = ($parent);
 		return; # continue $recv_cmd in background
 	}
-	if ($fds->[1]) {
+	if ($io->[1]) {
 		$pids{$pid} = undef;
 	} else {
 		$do_exec->(); # MUA reuses stdout
@@ -116,11 +112,11 @@ $SIG{CONT} = sub { send($sock, 'CONT', MSG_EOR) };
 
 my $x_it_code = 0;
 while (1) {
-	my (@fds) = $recv_cmd->($sock, my $buf, 4096 * 33);
-	die "recvmsg: $!" if scalar(@fds) == 1 && !defined($fds[0]);
+	my @io = $recv_cmd->($sock, my $buf, 4096 * 33);
+	die "recvmsg: $!" if scalar(@io) == 1 && !defined($io[0]);
 	last if $buf eq '';
 	if ($buf =~ /\Aexec (.+)\z/) {
-		$exec_cmd->(\@fds, split(/\0/, $1));
+		$exec_cmd->(\@io, split(/\0/, $1));
 	} elsif ($buf eq '-WINCH') {
 		kill($buf, @parent); # for MUA
 	} elsif ($buf eq 'umask') {

commit 21297c9b790d7adf0061162dd77e9152de63033a

spawn: fix send_cmd when no IO ref array is passed
We must use SvROK to check whether `sio' is usable, first,
otherwise SvRV will return a non-NULL value and the subsequent
av_len() call will segfault.  I only noticed this problem on
FreeBSD 14.x since Socket::Msghdr is not packaged for it.

Fixes: ddf589a5 (ipc: recv_cmd returns IO handles instead of FDs, 2026-02-28)

lib/PublicInbox/Spawn.pm
@@ -218,9 +218,10 @@ SV *send_cmd4_(PerlIO *s, SV *sio, SV *data, int flags, long tries)
 	STRLEN dlen = 0;
 	struct iovec iov;
 	ssize_t sent;
-	AV *io = (AV *)SvRV(sio);
-	I32 i, nfds = io ? (av_len(io) + 1) : 0;
+	I32 i;
 	int *fdp;
+	AV *io = SvROK(sio) ? (AV *)SvRV(sio) : NULL;
+	I32 nfds = io ? (I32)(av_len(io) + 1) : 0;
 
 	if (SvOK(data)) {
 		iov.iov_base = SvPV(data, dlen);